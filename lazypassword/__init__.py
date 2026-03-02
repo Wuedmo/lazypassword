8f50442d8b4e390c16ed27bdb71f57d40f44a917
WdmMaster <wdmmaster@users.noreply.github.com>
2026-03-02 06:40:37 -0500

    Export SSHManager and SSHKey from package __init__.py

--- lazypassword/__init__.py
+++ lazypassword/__init__.py
@@ -4,5 +4,17 @@ __version__ = "0.1.0"
 __author__ = "WdmMaster"
 
 from .utils import PasswordGenerator, ClipboardManager
+from .import_export import VaultImporter, VaultExporter, ImportFormat, DuplicateHandling
+from .ssh_manager import SSHManager, SSHKey
 
-__all__ = ["__version__", "PasswordGenerator", "ClipboardManager"]
+__all__ = [
+    "__version__",
+    "PasswordGenerator",
+    "ClipboardManager",
+    "VaultImporter",
+    "VaultExporter",
+    "ImportFormat",
+    "DuplicateHandling",
+    "SSHManager",
+    "SSHKey",
+]
